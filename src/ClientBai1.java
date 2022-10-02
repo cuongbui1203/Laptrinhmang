af3ad2d7dc8b3953cb27852d19a53586593a9a9d
monsieurtdn <83737033+monsieurtdn@users.noreply.github.com>
2022-10-02 11:46:45 +0700

    Create ClientBai1.java

---
+++ src/ClientBai1.java
@@ -0,0 +1,12 @@
+public class Client {
+
+public static void main(String[] args) {
+        try {
+            test2();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
