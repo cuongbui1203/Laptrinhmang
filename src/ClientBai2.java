f1cb6f2d80d605ac4b78605325e0c3ebb46e20ce
monsieurtdn <83737033+monsieurtdn@users.noreply.github.com>
2022-10-02 12:01:08 +0700

    Update ClientBai2.java

--- src/ClientBai2.java
+++ src/ClientBai2.java
@@ -2,8 +2,8 @@ public class ClientBai2 {
 
 public static void main(String[] args) {
         try {
-        //client bai quicksort
-            test2();
+        //client bai fibonacci
+            Client::test2();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
